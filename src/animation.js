15dcd1558d16c36dcbcbaba4b3f81c97cd533a60
joaolavelino <joaolavelino@gmail.com>
2022-01-13 14:24:55 -0300

    14. About Animation

--- src/animation.js
+++ src/animation.js
@@ -9,6 +9,7 @@ export const pageAnimation = {
     transition: {
       duration: 0.4,
       ease: "easeInOut",
+      staggerChildren: 0.1,
     },
   },
   exit: {
@@ -17,6 +18,41 @@ export const pageAnimation = {
     transition: {
       duration: 0.4,
       ease: "easeInOut",
+    },
+  },
+};
+
+export const titleAnimation = {
+  hidden: {
+    x: -200,
+    opacity: 0,
+  },
+  show: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      ease: "easeInOut",
+    },
+  },
+};
+
+export const fade = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: { ease: "easeOut", duration: 0.5 },
+  },
+};
+
+export const photoAnimation = {
+  hidden: { scale: 1.5, opacity: 0 },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.75,
     },
   },
 };
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,12 @@ import React from "react";
 import home1 from "../img/home1.png";
 import { Layout, Description, Image, Hide } from "../styles";
 import { motion } from "framer-motion";
+import {
+  titleAnimation,
+  dreamAnimation,
+  fade,
+  photoAnimation,
+} from "../animation";
 
 const AboutSection = () => {
   return (
@@ -9,30 +15,29 @@ const AboutSection = () => {
       <Description className="description">
         <div className="title">
           <Hide className="hide">
-            <motion.h2
-              animate={{ opacity: 1, transition: { duration: 2 } }}
-              initial={{ opacity: 0 }}
-            >
-              We work to make
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
+          </Hide>
+          <Hide className="hide">
+            <motion.h2 variants={titleAnimation}>
+              your <span>dreams</span>
             </motion.h2>
           </Hide>
           <Hide className="hide">
-            <h2>
-              your <span>dreams</span>
-            </h2>
-          </Hide>
-          <Hide className="hide">
-            <h2>come true.</h2>
+            <motion.h2 variants={titleAnimation}>come true.</motion.h2>
           </Hide>
         </div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image className="image">
-        <img src={home1} alt="Guy with a camera" />
+        <motion.img
+          variants={photoAnimation}
+          src={home1}
+          alt="Guy with a camera"
+        />
       </Image>
     </Layout>
   );
